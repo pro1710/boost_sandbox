5ae0971c437be2fb4d13cfef49681677d65cd8c9
rpykhniv <roman.pykhnivskyi@harmonicinc.com>
2022-07-17 22:31:08 +0300

    added net

---
+++ asio/NetCommon/net_message.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "net_common.h"
+
+namespace net {
+    template <typename T> 
+    struct message_header {
+        T id{};
+        uint32_t size = 0;
+    };
+
+    template <typename T> 
+    struct mesage {
+        message_header<T> header{};
+        std::vector<uint8_t> body;
+
+        size_t size() const {
+            return sizeof(message_header<T>) + body.size();
+        }
+    };
+
+    friend std::ostream& operator << (std::ostream& os, const message<T>& msg) {
+        os << "ID:" << static_cast<int>(message_header.id) 
+           << " Size:" << msg.header.size;
+        
+        return os;
+    }
+
+    template <typename DataType>
+    friend message<T>& operator << (message<T>& msg, const DataType& data) {
+        static_assert(std::is_standard_layout<DataType>::value, "Data doesn't have a standart layout");
+
+        size_t body_size = msg.body.size();
+
+        msg.body.resize(msg.body.size() + sizeof(DataType)); 
+
+        std::memcpy(msg.body.data() + body_size, &data, sizeof(DataType));
+
+        msg.header.size = msg.size();
+
+        return msg;
+    }
+
+    template <typename DataType>
+    friend message<T>& operator >> (message<T>& msg, DataType& data) {
+        static_assert(std::is_standard_layout<DataType>::value, "Data doesn't have a standart layout");
+
+        size_t loc = msg.body.size() - sizeof(DataType);
+
+        std::memcpy(&data, msg.body.data() + loc, sizeof(DataType));
+
+        msg.body.resize(loc);
+
+        msg.header.size = msg.size();
+
+        return msg;
+    }
+}
